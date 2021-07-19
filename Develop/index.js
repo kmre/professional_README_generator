3c2c4cb46704840bc8fe71ac20ab113b27a8d635
kmre <karla.romero7@gmail.com>
2021-07-19 12:23:03 -0400

    all questions added

--- Develop/index.js
+++ Develop/index.js
@@ -7,12 +7,12 @@ const mainQuestions = [
       {
         type: 'input',
         name: 'title',
-        message: 'What is the Title of your README file? (Required)',
+        message: 'What is the Title of your README file? (Required) \n',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-            console.log('Please enter a Title!');
+            console.log('Please enter a Title! \n');
             return false;
           }
         }
@@ -20,12 +20,12 @@ const mainQuestions = [
       {
         type: 'input',
         name: 'description',
-        message: 'Please enter a short description explaining the what, why, and how of your project (Required)',
+        message: 'Please enter a short description explaining the what, why, and how of your project (Required) \n',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-            console.log('Please enter a short description!');
+            console.log('Please enter a short description! \n');
             return false;
           }
         }
@@ -36,19 +36,19 @@ const multiQ1 = [
       {
         type: 'confirm',
         name: 'confirmInstallation',
-        message: 'Does your project require special instructions for installation?',
+        message: 'Does your project require special instructions for installation? \n',
         default: true
       },
       {
         type: 'input',
         name: 'installation',
-        message: 'What are the steps required to install your project?',
+        message: 'What are the steps required to install your project? \n',
         when: ({ confirmInstallation }) => confirmInstallation
       },
       {
         type: 'confirm',
         name: 'confirmQ1',
-        message: 'Would you like to enter another step?',
+        message: 'Would you like to enter another step? \n',
         when: ({ confirmInstallation }) => confirmInstallation,
         default: false
       }
@@ -58,12 +58,12 @@ const multiQ1 = [
       {
         type: 'input',
         name: 'usage',
-        message: 'Please provide instructions and examples for use. (Required)',
+        message: 'Please provide instructions and examples for use. (Required) \n',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-            console.log('Please enter a short list of instructions and uses!');
+            console.log('Please enter a short list of instructions and uses! \n');
             return false;
           }
         }
@@ -74,25 +74,25 @@ const multiQ1 = [
       {
         type: 'confirm',
         name: 'confirmCredits',
-        message: 'Do you want to add any collaborators or did you use any third-party assets you want to credit?',
+        message: 'Do you want to add any collaborators or did you use any third-party assets you want to credit? \n',
         default: true
       },
       {
         type: 'input',
         name: 'credits-name',
-        message: 'Enter the names of your collaborators or third-party assets',
+        message: 'Enter the names of your collaborators or third-party assets: \n',
         when: ({ confirmCredits }) => confirmCredits
       },
       {
         type: 'input',
         name: 'credits-git',
-        message: 'Enter the git username of your collaborators or url of third-party assets',
+        message: 'Enter the git username of your collaborators or url of third-party assets: \n',
         when: ({ confirmCredits }) => confirmCredits
       },
       {
         type: 'confirm',
         name: 'confirmQ3',
-        message: 'Would you like to enter another collaborator/asset?',
+        message: 'Would you like to enter another collaborator/asset? \n',
         when: ({ confirmCredits }) => confirmCredits,
         default: false
       }
@@ -102,13 +102,13 @@ const multiQ1 = [
         {
             type: 'confirm',
             name: 'confirmContributing',
-            message: 'Do you want other developers to contribute to your application?',
+            message: 'Do you want other developers to contribute to your application? \n',
             default: true
         },
         {
             type: 'input',
             name: 'contributing',
-            message: 'Add the guidelines you would like for them to follow',
+            message: 'Add the guidelines you would like for them to follow: \n',
             when: ({ confirmContributing }) => confirmContributing
         },
     ]
@@ -117,13 +117,13 @@ const multiQ1 = [
     {
         type: 'confirm',
         name: 'confirmFeatures',
-        message: 'Do you want to add any features to you README file?',
+        message: 'Do you want to add any features to you README file? \n',
         default: true
     },
     {
         type: 'input',
         name: 'features',
-        message: 'Enter key features of your project',
+        message: 'Enter key features of your project: \n',
         when: ({ confirmFeatures }) => confirmFeatures
     },
     ]
@@ -132,13 +132,13 @@ const multiQ1 = [
     {
         type: 'confirm',
         name: 'confirmTests',
-        message: 'Do you want other developers to test your application or do you have examples of tests you have performed?',
+        message: 'Do you want other developers to test your application or do you have examples of tests you have performed? \n',
         default: true
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'Provide examples on how to run them',
+        message: 'Provide examples on how to run them: \n',
         when: ({ confirmContributing }) => confirmContributing
     },
     ]
@@ -163,51 +163,37 @@ const multiQ1 = [
     //     default: true
     // },
     // {
-    //     type: 'input',
-    //     name: 'license',
-    //     message: 'Enter used licenses',
-    //     when: ({ confirmLicense }) => confirmLicense
-    // },
-    // {
-    //     type: 'confirm',
-    //     name: 'confirmBadge',
-    //     message: 'Do you want to add any badges to you README file?',
-    //     default: true
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'badge',
-    //     message: 'Enter used package name to generate the badge',
-    //     when: ({ confirmBadge }) => confirmBadge
-    // },
-    // //Need to loop through more than 1 feature
 
-    //     type: 'input',
-    //     name: 'username',
-    //     message: 'What is your GitHub username? (Required)',
-    //     validate: nameInput => {
-    //       if (nameInput) {
-    //         return true;
-    //       } else {
-    //         console.log('Please enter your username!');
-    //         return false;
-    //       }
-    //     }
-    //   },    
-    //   {
-    //     type: 'input',
-    //     name: 'email',
-    //     message: 'What is your email address? (Required)',
-    //     validate: nameInput => {
-    //       if (nameInput) {
-    //         return true;
-    //       } else {
-    //         console.log('Please enter your email address!');
-    //         return false;
-    //       }
-    //     }
-    //   },
-    //Need to loop for more than one test
+    const multiQ8 = [
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username? (Required) \n',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your username! \n');
+            return false;
+          }
+        }
+      },    
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required) \n',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your email address! \n');
+            return false;
+          }
+        }
+      },
+    ]
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -215,6 +201,11 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 //function init() {}
 const init = () => {
+    console.log(`
+    =================
+    Title and Description
+    =================
+    `);
     return inquirer.prompt(
         mainQuestions
     ); 
@@ -224,7 +215,7 @@ const init = () => {
 const promptQ1 = q1Data => {
     console.log(`
   =================
-  Add a New Step
+  Installation
   =================
   `);
     // If there's no 'questions' array property, create one
@@ -249,6 +240,11 @@ const promptQ1 = q1Data => {
 //For instruction right now it's not multiple input 
 //want to see if spaces can be used for multiple lines
     const promptQ2 = () => {
+        console.log(`
+        =================
+        Usage
+        =================
+        `);
         return inquirer.prompt(
             multiQ2
         ); 
@@ -258,7 +254,7 @@ const promptQ1 = q1Data => {
 const promptQ3 = q3Data => {
     console.log(`
   =================
-  Add a New Step
+  Credits
   =================
   `);
     // If there's no 'questions' array property, create one
@@ -281,18 +277,33 @@ const promptQ3 = q3Data => {
 //Prompt for multiple collabs
 
 const promptQ4 = () => {
+    console.log(`
+    =================
+    Contributing
+    =================
+    `);
     return inquirer.prompt(
         multiQ4
     ); 
 }
 
 const promptQ5 = () => {
+    console.log(`
+  =================
+  Features
+  =================
+  `);    
     return inquirer.prompt(
         multiQ5
     ); 
 }
 
 const promptQ6 = () => {
+    console.log(`
+    =================
+    Teststing
+    =================
+    `);
     return inquirer.prompt(
         multiQ6
     ); 
@@ -301,7 +312,7 @@ const promptQ6 = () => {
 const promptQ7 = q7Data => {
     console.log(`
   =================
-  Add a New Step
+  License
   =================
   `);
     // If there's no 'questions' array property, create one
@@ -321,14 +332,25 @@ const promptQ7 = q7Data => {
       });
   };
 
+  const promptQ8 = () => {
+    console.log(`
+  =================
+  Questions
+  =================
+  `);      
+    return inquirer.prompt(
+        multiQ8
+    ); 
+}
+
 
 // Function call to initialize app
  init()
-//  .then(promptQ1)
-//  .then(promptQ2)
-//  .then(promptQ3)
-//  .then(promptQ4)
-//  .then(promptQ5)
-//  .then(promptQ6)
-  .then(promptQ7)
-
+ .then(promptQ1)
+ .then(promptQ2)
+ .then(promptQ3)
+ .then(promptQ4)
+ .then(promptQ5)
+ .then(promptQ6)
+ .then(promptQ7)
+ .then(promptQ8)
