d9d9fa34633c0dfd35b22ea7ce5ce3412c47094e
kmre <karla.romero7@gmail.com>
2021-07-24 23:01:52 -0400

    git files added

--- Develop/src/readme-template.js
+++ Develop/src/readme-template.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // writing files
 const writeFile = readme => {
   return new Promise((resolve, reject) => {
-    fs.writeFile('../readme.md', readme, err => {
+    fs.writeFile('../readme2.md', readme, err => {
       if (err) {
         reject(err);
         return;
